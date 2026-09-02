eb64c4a9cab35467581e441398f1cb3dd5414307
eng-xitao <eng.wsouza@gmail.com>
2026-09-02 17:06:40 -0300

    style: ativa CSS dedicado da Central de Avaliações

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./theme.css";
 import "./prodpersonal-ui.css";
+import "./central-avaliacoes.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
