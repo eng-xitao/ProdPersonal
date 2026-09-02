a7f786976371ac39af5743f5768de214bd01bc54
eng-xitao <eng.wsouza@gmail.com>
2026-09-02 17:25:40 -0300

    Ativa ajustes globais de contraste e navegacao

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./theme.css";
 import "./prodpersonal-ui.css";
 import "./central-avaliacoes.css";
+import "./accessibility-ui.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
